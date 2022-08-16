e1373d539cc5a913f4bdbc2d20686d97a6df45ff
morevsavva <morevsavva@yandex-team.com>
2022-08-17 00:21:46 +0300

    Fix DivKit pods compilability
    Fix DivKit pods compilability

--- client/ios/DivKit/Extensions/DivTransformExtensions.swift
+++ client/ios/DivKit/Extensions/DivTransformExtensions.swift
@@ -3,7 +3,7 @@ import CoreGraphics
 import LayoutKit
 
 extension DivTransform {
-  public func makeAnchorPoint(expressionResolver: ExpressionResolver) -> AnchorPoint {
+  public func makeAnchorPoint(expressionResolver: ExpressionResolver) -> LayoutKit.AnchorPoint {
     AnchorPoint(
       x: pivotX.makeAnchorValue(expressionResolver: expressionResolver),
       y: pivotY.makeAnchorValue(expressionResolver: expressionResolver)
