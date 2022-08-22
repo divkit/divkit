1256096cd456b5546fc539acf4df7eb8e84bd392
gulevsky <gulevsky@yandex-team.com>
2022-08-22 16:17:06 +0300

    add gradle wrapper to kotlin json builder
    add gradle wrapper to kotlin json builder

---
+++ json-builder/kotlin/gradle/config/groovyc.groovy
@@ -0,0 +1,8 @@
+// Copyright (c) 2019 Yandex LLC. All rights reserved.
+// Author: Roman Golov <golov-rg@yandex-team.ru>
+
+import groovy.transform.CompileStatic
+
+withConfig(configuration) {
+    ast(CompileStatic)
+}
