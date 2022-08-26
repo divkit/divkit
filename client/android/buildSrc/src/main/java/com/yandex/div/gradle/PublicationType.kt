41b555f0d0bd83aad32d52134dc2f8291543bfe0
gulevsky <gulevsky@yandex-team.com>
2022-08-26 10:45:06 +0300

    configure publishing to maven central
    configure publishing to maven central

--- client/android/buildSrc/src/main/java/com/yandex/div/gradle/PublicationType.kt
+++ client/android/buildSrc/src/main/java/com/yandex/div/gradle/PublicationType.kt
@@ -3,26 +3,30 @@ package com.yandex.div.gradle
 import java.text.SimpleDateFormat
 import java.util.Date
 
+private const val RELEASE_MAVEN_CENTRAL_URL = "https://oss.sonatype.org/service/local/staging/deploy/maven2/"
+private const val SNAPSHOT_MAVEN_CENTRAL_URL = "https://oss.sonatype.org/content/repositories/snapshots/"
 private const val RELEASE_ARTIFACTORY_URL = "https://artifactory.yandex.net/artifactory/yandex_mobile_releases/"
 private const val SNAPSHOT_ARTIFACTORY_URL = "https://artifactory.yandex.net/artifactory/yandex_mobile_snapshots/"
 
 private val VERSION_DATE_FORMAT = SimpleDateFormat("yyyyMMdd.HHmmss")
 
-enum class PublicationType(val artifactoryUrl: String) {
+enum class PublicationType(
+    val mavenCentralUrl: String,
+    val artifactoryUrl: String
+) {
 
-    dev(SNAPSHOT_ARTIFACTORY_URL) {
-        override fun getVersionSuffix(properties: Map<String, Any>): String {
+    dev(SNAPSHOT_MAVEN_CENTRAL_URL, SNAPSHOT_ARTIFACTORY_URL) {
+        override fun getVersionSuffix(): String {
             val now = Date()
             return "-dev.${VERSION_DATE_FORMAT.format(now)}"
         }
     },
 
-    release(RELEASE_ARTIFACTORY_URL) {
-        override fun getVersionSuffix(properties: Map<String, Any>) = ""
+    release(RELEASE_MAVEN_CENTRAL_URL, RELEASE_ARTIFACTORY_URL) {
+        override fun getVersionSuffix() = ""
     };
 
-    // TODO: delete unused parameter
-    abstract fun getVersionSuffix(properties: Map<String, Any>): String
+    abstract fun getVersionSuffix(): String
 
     companion object {
 
