b4488015548267584adfad5f981a0c016c93fd7f
morevsavva <morevsavva@yandex-team.com>
2022-09-19 16:20:22 +0300

    Fix divkit pod compitability.
    Fix divkit pod compitability.

--- client/ios/DivKit/Extensions/DivTransformExtensions.swift
+++ client/ios/DivKit/Extensions/DivTransformExtensions.swift
@@ -1,7 +1,7 @@
 import LayoutKit
 
 extension DivTransform {
-  func makeAnchorPoint(expressionResolver: ExpressionResolver) -> AnchorPoint {
+  func makeAnchorPoint(expressionResolver: ExpressionResolver) -> LayoutKit.AnchorPoint {
     AnchorPoint(
       x: pivotX.makeAnchorValue(expressionResolver: expressionResolver),
       y: pivotY.makeAnchorValue(expressionResolver: expressionResolver)
