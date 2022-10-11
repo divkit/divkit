6cb1b16b83863ebda7cd91568ac86bfd1989f727
ventywing <ventywing@yandex-team.com>
2022-10-11 11:51:41 +0300

    Enable interactive tests for div-slider
    Enable interactive tests for div-slider

--- client/ios/DivKitSnapshotTests/DivInteractiveSnapshotTests.swift
+++ client/ios/DivKitSnapshotTests/DivInteractiveSnapshotTests.swift
@@ -1,14 +1,6 @@
 import XCTest
 
 private let exclusions: [String] = [
-  "div-slider/thumb-style/thumb-primary-style-properties.json",
-  "div-slider/thumb-style/thumb-secondary-style-properties.json",
-  "div-slider/thumb-values/thumb-values-with-tick-marks.json",
-  "div-slider/thumb-values/thumb-values-without-tick-marks.json",
-  "div-slider/tick-mark-style/tick-mark-active-style-properties.json",
-  "div-slider/tick-mark-style/tick-mark-inactive-style-properties.json",
-  "div-slider/track-style/track-active-style-properties.json",
-  "div-slider/track-style/track-inactive-style-properties.json",
   "div-gallery/select-elements.json",
   "div-pager/select-elements.json",
   "div-tabs/select-elements.json",
