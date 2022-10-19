a5e96b357650683f67bc3945a56882641b2aa5b8
4eb0da <4eb0da@yandex-team.com>
2022-10-19 17:38:59 +0300

    Update ts jsonbuilder build, remove generated sources from repo

--- json-builder/typescript/src/generated/DivLineStyle.ts
+++
@@ -1,9 +0,0 @@
-// Generated code. Do not modify.
-
-import { Exact, IntBoolean, NonEmptyArray } from '../helper';
-import { TemplateBlock, Type } from '../template';
-import { DivExpression } from '../safe-expression';
-
-export type DivLineStyle =
-    | 'none'
-    | 'single';
