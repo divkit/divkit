73bfe027cbc86acf95815b229d8c36f21e1ee7d8
pkurchatov <pkurchatov@yandex-team.com>
2022-12-07 15:28:09 +0300

    publish public release configuration

--- client/android/buildSrc/src/main/java/com/yandex/div/gradle/PublicationType.kt
+++ client/android/buildSrc/src/main/java/com/yandex/div/gradle/PublicationType.kt
@@ -3,26 +3,23 @@ package com.yandex.div.gradle
 import java.text.SimpleDateFormat
 import java.util.Date
 
-private const val RELEASE_MAVEN_CENTRAL_URL = "https://oss.sonatype.org/service/local/staging/deploy/maven2/"
-private const val SNAPSHOT_MAVEN_CENTRAL_URL = "https://oss.sonatype.org/content/repositories/snapshots/"
+private val VERSION_DATE_FORMAT = SimpleDateFormat("yyyyMMdd.HHmmss")
+
 private const val RELEASE_ARTIFACTORY_URL = "https://artifactory.yandex.net/artifactory/yandex_mobile_releases/"
 private const val SNAPSHOT_ARTIFACTORY_URL = "https://artifactory.yandex.net/artifactory/yandex_mobile_snapshots/"
 
-private val VERSION_DATE_FORMAT = SimpleDateFormat("yyyyMMdd.HHmmss")
-
 enum class PublicationType(
-    val mavenCentralUrl: String,
     val artifactoryUrl: String
 ) {
 
-    dev(SNAPSHOT_MAVEN_CENTRAL_URL, SNAPSHOT_ARTIFACTORY_URL) {
+    dev(SNAPSHOT_ARTIFACTORY_URL) {
         override fun getVersionSuffix(): String {
             val now = Date()
             return "-dev.${VERSION_DATE_FORMAT.format(now)}"
         }
     },
 
-    release(RELEASE_MAVEN_CENTRAL_URL, RELEASE_ARTIFACTORY_URL) {
+    release(RELEASE_ARTIFACTORY_URL) {
         override fun getVersionSuffix() = ""
     };
 
@@ -41,5 +38,4 @@ enum class PublicationType(
             }
         }
     }
-
 }
