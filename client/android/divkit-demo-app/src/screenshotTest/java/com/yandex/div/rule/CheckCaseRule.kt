778269731a9f93cd0cff36992f2e9db37d79f465
vyaivanove <vyaivanove@yandex-team.com>
2023-02-03 21:57:59 +0300

    Added support for 'description' field in screenshot tests

--- client/android/divkit-demo-app/src/screenshotTest/java/com/yandex/div/rule/CheckCaseRule.kt
+++ client/android/divkit-demo-app/src/screenshotTest/java/com/yandex/div/rule/CheckCaseRule.kt
@@ -5,7 +5,9 @@ import androidx.test.core.app.ApplicationProvider
 import com.yandex.div.internal.util.asList
 import com.yandex.divkit.demo.screenshot.DivAssetReader
 import com.yandex.test.rules.SimpleStatement
+import org.json.JSONArray
 import org.json.JSONObject
+import org.junit.Assert
 import org.junit.Assume
 import org.junit.rules.TestRule
 import org.junit.runner.Description
@@ -16,23 +18,38 @@ class CheckCaseRule(private val casePath: String) : TestRule {
 
     override fun apply(base: Statement, description: Description): Statement = SimpleStatement {
         val json = DivAssetReader(context).read(casePath)
-        val platforms = parsePlatforms(json)
 
-        Assume.assumeTrue("Skipping test, 'android' not listed as target platform", isForAndroidPlatform(platforms))
+        json.checkField<String>(DESCRIPTION_FIELD)
+        json.checkField<JSONArray>(PLATFORMS_FIELD)
+
+        checkPlatformSupported(json)
 
         base.evaluate()
     }
 
-    private fun parsePlatforms(json: JSONObject): List<String> {
-        return json.getJSONArray(PLATFORMS_FIELD).asList()
-    }
-
-    private fun isForAndroidPlatform(platforms: List<String>): Boolean {
-        return platforms.contains(VALUE_PLATFORM_ANDROID)
+    private fun checkPlatformSupported(json: JSONObject) {
+        val platforms = json.getJSONArray(PLATFORMS_FIELD).asList<String>()
+        Assume.assumeTrue(
+            "Skipping test, '$VALUE_PLATFORM_ANDROID' not listed as target platform",
+            platforms.contains(VALUE_PLATFORM_ANDROID)
+        )
     }
 
     companion object {
+        private inline fun <reified T> JSONObject.checkField(field: String) {
+            Assert.assertTrue(
+                "In test file '$field' field was not found",
+                has(field)
+            )
+            Assert.assertTrue(
+                "In test file '$DESCRIPTION_FIELD' is not ${T::class.simpleName}",
+                get(field) is T
+            )
+        }
+
         private const val PLATFORMS_FIELD = "platforms"
+        private const val DESCRIPTION_FIELD = "description"
+
         private const val VALUE_PLATFORM_ANDROID = "android"
     }
 }
