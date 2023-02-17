01fd9027bc5ddf9ffc68327f35c721420ce883ed
grechka62 <grechka62@yandex-team.com>
2023-02-17 18:26:49 +0300

    Fix transition animations for state layout (cherry-pick from 069f0e7ceb903858c14ed5a5ca1a5b42aab759bf)

--- client/android/div/src/main/java/com/yandex/div/core/view2/divs/widgets/DivStateLayout.kt
+++ client/android/div/src/main/java/com/yandex/div/core/view2/divs/widgets/DivStateLayout.kt
@@ -20,6 +20,8 @@ import com.yandex.div.core.expression.ExpressionSubscriber
 import com.yandex.div.core.state.DivStatePath
 import com.yandex.div.core.view2.divs.drawChildrenShadows
 import com.yandex.div.core.view2.divs.updateBorderDrawer
+import com.yandex.div.core.widget.invalidateAfter
+import com.yandex.div.internal.widget.TransientView
 import com.yandex.div.json.expressions.ExpressionResolver
 import com.yandex.div2.Div
 import com.yandex.div2.DivBorder
@@ -33,7 +35,7 @@ internal class DivStateLayout @JvmOverloads constructor(
     context: Context,
     attrs: AttributeSet? = null,
     defStyleAttr: Int = 0
-) : FrameLayout(context, attrs, defStyleAttr), DivBorderSupports, ExpressionSubscriber {
+) : FrameLayout(context, attrs, defStyleAttr), DivBorderSupports, ExpressionSubscriber, TransientView {
 
     var path: DivStatePath? = null
     val stateId: String?
@@ -43,6 +45,11 @@ internal class DivStateLayout @JvmOverloads constructor(
     var swipeOutCallback: (() -> Unit)? = null
     internal var divState: DivState? = null
     internal var activeStateDiv: Div? = null
+
+    override var isTransient = false
+        set(value) = invalidateAfter {
+            field = value
+        }
 
     private var borderDrawer: DivBorderDrawer? = null
     override val border: DivBorder?
