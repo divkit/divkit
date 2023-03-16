d35d264619cd74f0b9761fe23b423e4423fc52a2
grechka62 <grechka62@yandex-team.com>
2023-03-16 14:07:45 +0300

    Fix scroll view for demo-app

---
+++ client/android/divkit-demo-app/src/main/java/com/yandex/divkit/demo/div/DemoNestedScrollView.kt
@@ -0,0 +1,35 @@
+package com.yandex.divkit.demo.div
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+import androidx.core.widget.NestedScrollView
+
+class DemoNestedScrollView @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0
+) : NestedScrollView(context, attrs, defStyleAttr) {
+
+    override fun measureChild(child: View, parentWidthMeasureSpec: Int, parentHeightMeasureSpec: Int) {
+        val lp = child.layoutParams as MarginLayoutParams
+
+        val childWidthMeasureSpec: Int = getChildMeasureSpec(parentWidthMeasureSpec,
+            paddingLeft + paddingRight, lp.width)
+        val childHeightMeasureSpec: Int = getChildMeasureSpec(parentHeightMeasureSpec,
+            paddingTop + paddingBottom, lp.height)
+
+        child.measure(childWidthMeasureSpec, childHeightMeasureSpec)
+    }
+
+    override fun measureChildWithMargins(child: View, parentWidthMeasureSpec: Int, widthUsed: Int, parentHeightMeasureSpec: Int, heightUsed: Int) {
+        val lp = child.layoutParams as MarginLayoutParams
+
+        val childWidthMeasureSpec = getChildMeasureSpec(parentWidthMeasureSpec,
+            paddingLeft + paddingRight + lp.leftMargin + lp.rightMargin + widthUsed, lp.width)
+        val childHeightMeasureSpec = getChildMeasureSpec(parentHeightMeasureSpec,
+            paddingTop + paddingBottom + lp.topMargin + lp.bottomMargin + heightUsed, lp.height)
+
+        child.measure(childWidthMeasureSpec, childHeightMeasureSpec)
+    }
+}
