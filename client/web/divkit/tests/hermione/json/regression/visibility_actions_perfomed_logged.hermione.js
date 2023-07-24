fc3f46c12025b0e899f8c66900295534d4aa7ac1
skvortsov-den <skvortsov-den@yandex-team.com>
2023-07-24 16:43:39 +0300

    Test Hermione Visibility actions performed and logged in pager

---
+++ client/web/divkit/tests/hermione/json/regression/visibility_actions_perfomed_logged.hermione.js
@@ -0,0 +1,27 @@
+describe('regression', () => {
+    describe('Visibility actions performed and logged in pager', () => {
+        beforeEach(async function() {
+            await this.browser.execute(() => {
+                window.divkitLogs = [];
+            });
+            await this.browser.yaOpenRegressionJson('visibility_actions/swipe_div_pager');
+        });
+
+        hermione.only.in('chromeMobile', 'pointerType="touch" is not supported on firefox');
+        it('Horizontal swipe grouped bulkAction', async function() {
+            // Wait for visibility action logging
+            await this.browser.pause(1000);
+            await this.browser.yaSwipe({
+                selector: 'div[role=tabpanel]',
+                direction: 'left',
+                duration: 500,
+                offset: 300
+            });
+            await this.browser.assertView('swipe_left_bulkAction', '#root');
+
+            let logs = await this.browser.execute(() => window.divkitLogs);
+            logs = logs.map(log => log.action.log_id);
+            logs.should.contain('6','content_item_show:0','106','content_item_show:1');
+        });
+    });
+});
