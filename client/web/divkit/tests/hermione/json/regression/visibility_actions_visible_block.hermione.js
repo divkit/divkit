b82f48ce7795e15f9d3bccef2095228318e46820
skvortsov-den <skvortsov-den@yandex-team.com>
2023-07-26 11:43:32 +0300

    Test Visibility action in visible block

---
+++ client/web/divkit/tests/hermione/json/regression/visibility_actions_visible_block.hermione.js
@@ -0,0 +1,14 @@
+describe('regression', () => {
+    describe('Visibility action in visible block', () => {
+        beforeEach(async function() {
+            await this.browser.yaOpenRegressionJson('visibility_actions/visibility_aciton');
+        });
+
+        it('Actions in in visible block', async function() {
+            await this.browser.assertView('screen_visible_block_1', '#root');
+            // Wait 2 seconds
+            await this.browser.pause(2000);
+            await this.browser.assertView('screen_visible_block_2', '#root');
+        });
+    });
+});
