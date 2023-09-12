68cf0df463777e96e4fa170b6e68ca1614f05b86
morevsavva <morevsavva@yandex-team.com>
2023-09-12 23:41:57 +0300

    fixes calculating container height with constrained children.

--- client/ios/LayoutKit/LayoutKit/Blocks/IntrincisHeights.swift
+++ client/ios/LayoutKit/LayoutKit/Blocks/IntrincisHeights.swift
@@ -3,10 +3,15 @@ import CoreGraphics
 extension Array where Element == Block {
   func intrinsicHeights(forWidth width: CGFloat) -> [CGFloat] {
     map {
-      let widthConstraint = $0.isHorizontallyResizable
-        ? width
-        : $0.widthOfHorizontallyNonResizableBlock
-      return $0.intrinsicContentHeight(forWidth: widthConstraint)
+      let blockWidth: CGFloat
+      if $0.isHorizontallyResizable {
+        blockWidth = width
+      } else if $0.isHorizontallyConstrained {
+        blockWidth = Swift.min($0.widthOfHorizontallyNonResizableBlock, width)
+      } else {
+        blockWidth = $0.widthOfHorizontallyNonResizableBlock
+      }
+      return $0.intrinsicContentHeight(forWidth: blockWidth)
     }
   }
 }
