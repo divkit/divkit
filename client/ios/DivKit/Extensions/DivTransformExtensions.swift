55a55f266fedb0e0a65ed9bfb5772da2be512396
pkurchatov <pkurchatov@yandex-team.com>
2023-11-27 20:11:13 +0300

    Resolved AnchorPoint type name conflict

--- client/ios/DivKit/Extensions/DivTransformExtensions.swift
+++ client/ios/DivKit/Extensions/DivTransformExtensions.swift
@@ -1,8 +1,9 @@
 import LayoutKit
 
 extension DivTransform {
-  func resolveAnchorPoint(_ expressionResolver: ExpressionResolver) -> AnchorPoint {
-    AnchorPoint(
+  func resolveAnchorPoint(_ expressionResolver: ExpressionResolver) -> LayoutKit.AnchorPoint {
+    // must keep package name here due to conflict with CoreText.AnchorPoint
+    LayoutKit.AnchorPoint(
       x: pivotX.resolveAnchorValue(expressionResolver),
       y: pivotY.resolveAnchorValue(expressionResolver)
     )
