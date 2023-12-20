4b47890966946bc574b2d4558ef5e3ecda69c70b
gulevsky <gulevsky@yandex-team.com>
2023-12-20 17:54:31 +0300

    optimize div separator binding

--- client/android/div/src/main/java/com/yandex/div/core/view2/divs/DivSeparatorBinder.kt
+++ client/android/div/src/main/java/com/yandex/div/core/view2/divs/DivSeparatorBinder.kt
@@ -8,6 +8,8 @@ import com.yandex.div.core.view2.Div2View
 import com.yandex.div.core.view2.DivViewBinder
 import com.yandex.div.core.view2.divs.widgets.DivSeparatorView
 import com.yandex.div.json.expressions.ExpressionResolver
+import com.yandex.div.json.expressions.equalsToConstant
+import com.yandex.div.json.expressions.isConstant
 import com.yandex.div2.DivSeparator
 import javax.inject.Inject
 
@@ -20,35 +22,39 @@ internal class DivSeparatorBinder @Inject constructor(
         val oldDiv = view.div
         if (div == oldDiv) return
 
-        val expressionResolver = divView.expressionResolver
-
         baseBinder.bindView(view, div, oldDiv, divView)
-
         view.applyDivActions(divView, div.action, div.actions, div.longtapActions, div.doubletapActions, div.actionAnimation)
 
-        view.applyStyle(div.delimiterStyle, expressionResolver)
+        view.bindStyle(div.delimiterStyle, oldDiv?.delimiterStyle, divView.expressionResolver)
 
         view.setDividerHeightResource(R.dimen.div_separator_delimiter_height)
         view.dividerGravity = Gravity.CENTER
     }
 
-    private fun DivSeparatorView.applyStyle(style: DivSeparator.DelimiterStyle?, resolver: ExpressionResolver) {
-        val colorExpr = style?.color
-        if (colorExpr == null) {
-            dividerColor = Color.TRANSPARENT
-        } else {
-            addSubscription(colorExpr.observeAndGet(resolver) { color -> dividerColor = color })
+    private fun DivSeparatorView.bindStyle(
+        newStyle: DivSeparator.DelimiterStyle,
+        oldStyle: DivSeparator.DelimiterStyle?,
+        resolver: ExpressionResolver
+    ) {
+        if (newStyle.color.equalsToConstant(oldStyle?.color)
+            && newStyle.orientation.equalsToConstant(oldStyle?.orientation)) {
+            return
         }
 
-        val orientationExpr = style?.orientation
-        if (orientationExpr == null) {
-            isHorizontal = false
-        } else {
-            addSubscription(
-                orientationExpr.observeAndGet(resolver) { orientation ->
-                    isHorizontal = orientation == DivSeparator.DelimiterStyle.Orientation.HORIZONTAL
-                }
-            )
+        applyStyle(newStyle, resolver)
+
+        if (newStyle.color.isConstant() && newStyle.orientation.isConstant()) {
+            return
         }
+
+        val callback = { _: Any -> applyStyle(newStyle, resolver) }
+        addSubscription(newStyle.color.observe(resolver, callback))
+        addSubscription(newStyle.orientation.observe(resolver, callback))
+    }
+
+    private fun DivSeparatorView.applyStyle(style: DivSeparator.DelimiterStyle?, resolver: ExpressionResolver) {
+        dividerColor = style?.color?.evaluate(resolver) ?: Color.TRANSPARENT
+        val orientation = style?.orientation?.evaluate(resolver) ?: DivSeparator.DelimiterStyle.Orientation.VERTICAL
+        isHorizontal = orientation == DivSeparator.DelimiterStyle.Orientation.HORIZONTAL
     }
 }
