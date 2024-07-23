b4abb7a230af2def74f17be83f4b7fe3b8209a09
morevsavva <morevsavva@yandex-team.com>
2024-07-23 20:06:21 +0300

    removed legacy Sample App. 8af91b87ffae26ce7114c7a32eb7f373035ee9dc

--- client/ios/Sample/DivKitSample/DivHostViewController.swift
+++ client/ios/Sample/DivKitSample/DivHostViewController.swift
@@ -2,30 +2,4 @@ import UIKit
 
 import DivKit
 
-final class DivHostViewController: UIViewController {
-  private var divHostView: DivHostView!
-  private var components: DivKitComponents!
-
-  override func viewDidLoad() {
-    super.viewDidLoad()
-    components = DivKitComponents(
-      urlHandler: DivUrlHandlerDelegate { UIApplication.shared.open($0) }
-    )
-    let preloader = DivViewPreloader(divKitComponents: components)
-    divHostView = DivHostView(components: components, preloader: preloader)
-
-    if let cards = try? DivJson.loadCards() {
-      view.addSubview(divHostView)
-      Task {
-        await preloader
-          .setSources(cards.map { DivViewSource(kind: .divData($0), cardId: $0.cardId) })
-        divHostView.items = cards.map(\.cardId)
-      }
-    }
-  }
-
-  override func viewDidLayoutSubviews() {
-    super.viewDidLayoutSubviews()
-    divHostView.frame = view.bounds.inset(by: view.safeAreaInsets)
-  }
-}
+final class DivHostViewController: UIViewController { }
