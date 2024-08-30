ebf8dba0c1ac61d2775ed6090aa805474e827e43
saabrosov <saabrosov@yandex-team.com>
2024-08-30 14:31:45 +0300

    Fixed a bug with scrolling through the last page. 6a2f4edd090ae57739bbb41652f3332150ec2cf6

--- client/ios/LayoutKit/LayoutKit/UI/Base/ScrollableContentPager.swift
+++ client/ios/LayoutKit/LayoutKit/UI/Base/ScrollableContentPager.swift
@@ -24,54 +24,60 @@ public final class ScrollableContentPager: NSObject {
   public func setInitialOffset(_ offset: CGFloat) {
     guard isPagingEnabled else { return }
     
-    if currentPageIndex == nil, indexedPageOrigins.count > 0 {
-      currentPageIndex = pageIndex(forOffset: offset)
+    if currentPageIndex == nil, lastContentOffset == nil, indexedPageOrigins.count > 0 {
+      let initialPageIndex = pageIndex(forOffset: offset)
+      let pageOrigin = indexedPageOrigins[initialPageIndex].origin
+      let initialContentOffset = isHorizontal ? CGPoint(x: pageOrigin, y: 0) : CGPoint(x: 0, y: pageOrigin)
+      
+      currentPageIndex = initialPageIndex
+      lastContentOffset = initialContentOffset
     }
   }
 
   public private(set) var currentPageIndex: Int?
+  public private(set) var lastContentOffset: CGPoint?
 
-  public func targetPageOffset(
-    forProposedOffset offset: CGFloat,
-    direction: CGPoint,
-    isHorizontal: Bool
-  ) -> CGFloat? {
-    guard let currentIndex = currentPageIndex else {
+  public func targetPageOffset(forProposedOffset offset: CGFloat, isHorizontal: Bool) -> CGFloat? {
+    guard currentPageIndex != nil,
+          var newLastContentOffset = lastContentOffset else {
       return nil
     }
     
-    let forcedIndexOffset = direction.forceIndexOffset(isHorizontal: isHorizontal) > 0 ? -1 : 0
-    var resultPageIndex = pageIndex(forOffset: offset) + forcedIndexOffset
-    
-    // If we scrolled to the first/last page, which may be "small" and try to scroll to the next/previous page,
-    // we may be thrown to 2 pages instead of the required 1 page.
-    // These checks reduce the "sensitivity" of scrolling on corner cases and shift us to 1 page instead of 2 pages.
-    if currentPageIndex == indexedPageOrigins.first?.index && resultPageIndex - currentIndex == 2 {
-      resultPageIndex = currentIndex + 1
-    } else if currentPageIndex == indexedPageOrigins.last?.index && currentIndex - resultPageIndex == 2 {
-      resultPageIndex = currentIndex - 1
+    var resultPageIndex = pageIndex(forOffset: offset)
+
+    var forcedIndexOffset = 0
+    if newLastContentOffset.forceIndexOffset(isHorizontal: isHorizontal).isApproximatelyNotEqualTo(offset) {
+      forcedIndexOffset = newLastContentOffset.forceIndexOffset(isHorizontal: isHorizontal) < offset ? 1 : 0
     }
+    resultPageIndex += forcedIndexOffset
     
     resultPageIndex = clamp(
       resultPageIndex,
       min: 0,
       max: indexedPageOrigins.count - 1
     )
-    currentPageIndex = resultPageIndex
     
-    return indexedPageOrigins[resultPageIndex].origin
+    let targetPageOffset = indexedPageOrigins[resultPageIndex].origin
+    if isHorizontal {
+      newLastContentOffset.x = targetPageOffset
+    } else {
+      newLastContentOffset.y = targetPageOffset
+    }
+    lastContentOffset = newLastContentOffset
+    currentPageIndex = resultPageIndex
+    return targetPageOffset
   }
   
   private func pageIndex(forOffset offset: CGFloat) -> Int {
-    guard let rightBound = indexedPageOrigins.first(where: {
+    guard indexedPageOrigins.first(where: {
       $0.origin.isApproximatelyGreaterOrEqualThan(offset)
-    }) else { return indexedPageOrigins.count - 1 }
+    }) != nil else { return indexedPageOrigins.count - 1 }
     
-    guard indexedPageOrigins.reversed().first(where: {
+    guard let leftBound = indexedPageOrigins.reversed().first(where: {
       $0.origin.isApproximatelyLessOrEqualThan(offset)
-    }) != nil else { return 0 }
+    }) else { return 0 }
     
-    return rightBound.index
+    return leftBound.index
   }
 }
 
--- client/ios/LayoutKit/LayoutKitTests/UI/Base/ScrollableContentPagerTests.swift
+++ client/ios/LayoutKit/LayoutKitTests/UI/Base/ScrollableContentPagerTests.swift
@@ -21,7 +21,7 @@ final class ScrollableContentPagerTests: XCTestCase {
   func test_WhenMovingForward_SnapsToNextPage() {
     pager.setInitialOffset(20)
 
-    let resultOffset = pager.targetPageOffset(forProposedOffset: 170, direction: CGPoint(x: -1.0, y: -1.0), isHorizontal: isHorizontal)!
+    let resultOffset = pager.targetPageOffset(forProposedOffset: 170, isHorizontal: isHorizontal)!
 
     XCTAssertEqual(resultOffset, 250)
   }
@@ -29,7 +29,7 @@ final class ScrollableContentPagerTests: XCTestCase {
   func test_WhenMovingForwardAndResultPageIsSame_ForcesToNextPage() {
     pager.setInitialOffset(20)
 
-    let resultOffset = pager.targetPageOffset(forProposedOffset: 49, direction: CGPoint(x: -1.0, y: -1.0), isHorizontal: isHorizontal)!
+    let resultOffset = pager.targetPageOffset(forProposedOffset: 49, isHorizontal: isHorizontal)!
 
     XCTAssertEqual(resultOffset, 100)
   }
@@ -37,7 +37,7 @@ final class ScrollableContentPagerTests: XCTestCase {
   func test_WhenMovingBackward_SnapsToPreviousPage() {
     pager.setInitialOffset(260)
 
-    let resultOffset = pager.targetPageOffset(forProposedOffset: 49, direction: CGPoint(x: 1.0, y: 1.0), isHorizontal: isHorizontal)!
+    let resultOffset = pager.targetPageOffset(forProposedOffset: 49, isHorizontal: isHorizontal)!
 
     XCTAssertEqual(resultOffset, 0) // over one page
   }
@@ -45,7 +45,7 @@ final class ScrollableContentPagerTests: XCTestCase {
   func test_WhenMovingBackwardAndResultPageIsSame_ForcesToPreviousPage() {
     pager.setInitialOffset(110)
 
-    let resultOffset = pager.targetPageOffset(forProposedOffset: 51, direction: CGPoint(x: 1.0, y: 1.0), isHorizontal: isHorizontal)!
+    let resultOffset = pager.targetPageOffset(forProposedOffset: 51, isHorizontal: isHorizontal)!
 
     XCTAssertEqual(resultOffset, 0)
   }
@@ -53,7 +53,7 @@ final class ScrollableContentPagerTests: XCTestCase {
   func test_WhenBouncingLeft_SnapsToFirstPage() {
     pager.setInitialOffset(110)
 
-    let resultOffset = pager.targetPageOffset(forProposedOffset: -10, direction: CGPoint(x: 1.0, y: 1.0), isHorizontal: isHorizontal)!
+    let resultOffset = pager.targetPageOffset(forProposedOffset: -10, isHorizontal: isHorizontal)!
 
     XCTAssertEqual(resultOffset, 0)
   }
@@ -61,7 +61,7 @@ final class ScrollableContentPagerTests: XCTestCase {
   func test_WhenBouncingRight_SnapsToLastPage() {
     pager.setInitialOffset(300)
 
-    let resultOffset = pager.targetPageOffset(forProposedOffset: 400, direction: CGPoint(x: -1.0, y: -1.0), isHorizontal: isHorizontal)!
+    let resultOffset = pager.targetPageOffset(forProposedOffset: 400, isHorizontal: isHorizontal)!
 
     XCTAssertEqual(resultOffset, 300)
   }
@@ -70,13 +70,13 @@ final class ScrollableContentPagerTests: XCTestCase {
     pager.setPageOrigins([100, 250], withPagingEnabled: false, isHorizontal: isHorizontal)
     pager.setInitialOffset(100)
 
-    let resultOffset = pager.targetPageOffset(forProposedOffset: 150, direction: CGPoint(x: 1.0, y: 1.0), isHorizontal: isHorizontal)
+    let resultOffset = pager.targetPageOffset(forProposedOffset: 150, isHorizontal: isHorizontal)
 
     XCTAssertNil(resultOffset)
   }
 
   func test_WithNoInitialOffset_TargetPageOffsetFails() {
-    let resultOffset = pager.targetPageOffset(forProposedOffset: 150, direction: CGPoint(x: 1.0, y: 1.0), isHorizontal: isHorizontal)
+    let resultOffset = pager.targetPageOffset(forProposedOffset: 150, isHorizontal: isHorizontal)
 
     XCTAssertNil(resultOffset)
   }
