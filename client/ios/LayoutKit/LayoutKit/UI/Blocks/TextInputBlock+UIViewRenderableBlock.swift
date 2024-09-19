47326aed2b45f4107dec94699654d6173797c447
burstein <burstein@yandex-team.com>
2024-09-19 19:20:26 +0300

    Fixed multiline input keyboard commit_hash:1fa4f6240e3ade8d12d9220a378380404f4e8ec4

--- client/ios/LayoutKit/LayoutKit/UI/Blocks/TextInputBlock+UIViewRenderableBlock.swift
+++ client/ios/LayoutKit/LayoutKit/UI/Blocks/TextInputBlock+UIViewRenderableBlock.swift
@@ -122,6 +122,7 @@ private final class TextInputBlockView: BlockView, VisibleBoundsTrackingLeaf {
 
     multiLineInput.frame = bounds
     multiLineInput.textContainerInset = paddings
+    updateScrollOnMultilineChange()
     updateMultiLineOffset()
 
     singleLineInput.frame = bounds
@@ -432,9 +433,17 @@ private final class TextInputBlockView: BlockView, VisibleBoundsTrackingLeaf {
   private func tryScrollToMultiLine(_ keyboardHeight: CGFloat) {
     guard !scrollingWasDone, multiLineInput.isFirstResponder,
           !multiLineInput.isHidden else { return }
-    let frameInWindow = multiLineInput.convert(multiLineInput.frame, to: nil)
-    let cursorPoint = frameInWindow.origin.y + multiLineInput.contentSize
-      .height - multiLineOffsetY + additionalOffset
+
+    let frameInWindow = if let textRange = multiLineInput.selectedTextRange {
+      multiLineInput.convert(
+        multiLineInput.caretRect(for: textRange.start),
+        to: nil
+      )
+    } else {
+      multiLineInput.convert(multiLineInput.bounds, to: nil)
+    }
+
+    let cursorPoint = frameInWindow.origin.y + additionalOffset
     scrollToVisible(targetY: cursorPoint, keyboardHeight: keyboardHeight)
   }
 
@@ -822,6 +831,6 @@ private class PatchedUITextView: UITextView {
   }
 }
 
-private let additionalOffset = 25.0
+private let additionalOffset = 40.0
 private let singleLineCusorOffset = 2.0
 private let multiLineCusorOffset = 0.0
