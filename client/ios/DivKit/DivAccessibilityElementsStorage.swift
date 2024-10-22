1865cd93aec8dbe9ee583972d51b03594e9ca93a
ventywing <ventywing@yandex-team.com>
2024-10-22 18:11:33 +0300

    Fix build for browser commit_hash:df03dcb70caf73c013c0a62d9d0a3f9d0442f548

--- client/ios/DivKit/DivAccessibilityElementsStorage.swift
+++ client/ios/DivKit/DivAccessibilityElementsStorage.swift
@@ -1,59 +1,50 @@
 import VGSL
 
-#if os(iOS)
-import UIKit
-#else
-import AppKit
-#endif
-
 final class DivAccessibilityElementsStorage {
   private var ids = [String]()
   private var idToForwardId = [String: String]()
-  
+
   func put(id: String, nextId: String) {
     ids.append(id)
     idToForwardId[id] = nextId
   }
-  
+
   func getOrderedIds() -> [String] {
     var orderedIds = [String]()
     var idsSet = Set<String>()
-    
+
     for elementId in ids {
       if idsSet.contains(elementId) {
         continue
       }
-      
+
       var nodeId = elementId
       while !idsSet.contains(nodeId) {
         orderedIds.append(nodeId)
         idsSet.insert(nodeId)
-        
+
         if let nextNodeId = idToForwardId[nodeId] {
           nodeId = nextNodeId
         }
       }
     }
-    
+
     return orderedIds
   }
-  
+
+  #if os(iOS)
   func getAccessibilityElements(from view: ViewType) -> [Any]? {
-    #if !os(iOS)
-      return nil
-    #endif
-    
     let orderedIds = getOrderedIds()
-    
+
     guard !orderedIds.isEmpty else {
       return view.superview?.accessibilityElements
     }
-    
+
     var systemElements = [Any]()
     if let parentElements = view.superview?.accessibilityElements {
       systemElements = parentElements
     }
-    
+
     var elements = [Any]()
     for id in orderedIds {
       if let element = findElement(withID: id, in: view),
@@ -61,24 +52,19 @@ final class DivAccessibilityElementsStorage {
          !subview.isHidden,
          subview.alpha > 0,
          subview.frame != .zero,
-         subview.isDescendant(of: view)
-      {
+         subview.isDescendant(of: view) {
         elements.append(contentsOf: [element])
         systemElements.removeAll {
           ($0 as? ViewType)?.accessibilityIdentifier == id
         }
       }
     }
-    
+
     elements += systemElements
     return elements
   }
-  
+
   private func findElement(withID id: String, in view: ViewType) -> Any? {
-    #if !os(iOS)
-      return nil
-    #endif
-    
     if view.accessibilityIdentifier == id {
       return view
     }
@@ -89,4 +75,9 @@ final class DivAccessibilityElementsStorage {
     }
     return nil
   }
+  #else
+  func getAccessibilityElements(from _: ViewType) -> [Any]? {
+    nil
+  }
+  #endif
 }
