eb2a365f1f444ed704c07c26b797682d2c1485ed
pkurchatov <pkurchatov@yandex-team.com>
2024-12-05 01:04:01 +0300

    Fixed layout of items with constrained size in overlapped container commit_hash:b9f979127e462fca52be4452116fa8d2c6e1a4bd

--- client/ios/LayoutKit/LayoutKit/Blocks/Block.swift
+++ client/ios/LayoutKit/LayoutKit/Blocks/Block.swift
@@ -114,11 +114,13 @@ extension Block {
   }
 
   public func size(forResizableBlockSize resizableBlockSize: CGSize) -> CGSize {
-    sizeFor(
-      widthOfHorizontallyResizableBlock: resizableBlockSize.width,
-      heightOfVerticallyResizableBlock: resizableBlockSize.height,
-      constrainedWidth: .infinity,
-      constrainedHeight: .infinity
+    let width = resizableBlockSize.width
+    let height = resizableBlockSize.height
+    return sizeFor(
+      widthOfHorizontallyResizableBlock: width,
+      heightOfVerticallyResizableBlock: height,
+      constrainedWidth: isHorizontallyConstrained ? width : .infinity,
+      constrainedHeight: isVerticallyConstrained ? height : .infinity
     )
   }
 
