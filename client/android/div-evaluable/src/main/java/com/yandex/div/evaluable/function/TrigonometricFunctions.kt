8fd0d2fd3bd9c9fdecbfa35fb5091f8c3983ebb8
bagrusss <>
2024-12-14 19:45:09 +0300

    issue #74 cos function implementation

--- client/android/div-evaluable/src/main/java/com/yandex/div/evaluable/function/TrigonometricFunctions.kt
+++ client/android/div-evaluable/src/main/java/com/yandex/div/evaluable/function/TrigonometricFunctions.kt
@@ -5,6 +5,7 @@ import com.yandex.div.evaluable.EvaluationContext
 import com.yandex.div.evaluable.ExpressionContext
 import com.yandex.div.evaluable.Function
 import com.yandex.div.evaluable.FunctionArgument
+import kotlin.math.cos
 import kotlin.math.sin
 
 internal object Pi : Function() {
@@ -62,5 +63,20 @@ internal object Sine : Function() {
         args: List<Any>,
     ): Any {
         return sin(args.first() as Double)
+    }
+}
+
+internal object Cos: Function() {
+    override val name = "cos"
+    override val declaredArgs = listOf(FunctionArgument(type = EvaluableType.NUMBER))
+    override val resultType = EvaluableType.NUMBER
+    override val isPure = true
+
+    override fun evaluate(
+        evaluationContext: EvaluationContext,
+        expressionContext: ExpressionContext,
+        args: List<Any>,
+    ): Any {
+        return cos(args.first() as Double)
     }
 }
