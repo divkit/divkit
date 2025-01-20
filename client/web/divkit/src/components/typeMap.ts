3c6aebd59642c4284f0aa6bb3fce1bfeac31af90
4eb0da <4eb0da@yandex-team.com>
2025-01-20 16:50:08 +0300

    Fix build commit_hash:595886c44b59b880c74297ff3657b578423bdd93

--- client/web/divkit/src/components/typeMap.ts
+++ client/web/divkit/src/components/typeMap.ts
@@ -1,4 +1,6 @@
 import type { ComponentType } from 'svelte';
+// FOrce import order
+import './utilities/Outer.svelte';
 import Text from './text/Text.svelte';
 import Container from './container/Container.svelte';
 import Separator from './separator/Separator.svelte';
