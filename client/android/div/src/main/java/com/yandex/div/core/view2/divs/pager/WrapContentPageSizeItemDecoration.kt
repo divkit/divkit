a1547d548170b42176b7f633ff53473b280f26c8
grechka62 <grechka62@yandex-team.com>
2025-02-03 11:53:46 +0300

    Support scroll_axis_alignment in pager commit_hash:f2203b578cd9751da6f25529298d26af2934dc96

--- client/android/div/src/main/java/com/yandex/div/core/view2/divs/pager/WrapContentPageSizeItemDecoration.kt
+++ client/android/div/src/main/java/com/yandex/div/core/view2/divs/pager/WrapContentPageSizeItemDecoration.kt
@@ -4,27 +4,47 @@ import android.graphics.Rect
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView
 import com.yandex.div.core.widget.makeUnspecifiedSpec
+import com.yandex.div2.DivPager.ItemAlignment
 import kotlin.math.roundToInt
 
 internal class WrapContentPageSizeItemDecoration(
     private val parentSize: Int,
-    paddings: DivPagerPaddingsHolder,
-    isHorizontal: Boolean,
+    private val paddings: DivPagerPaddingsHolder,
+    private val alignment: ItemAlignment,
 ) : RecyclerView.ItemDecoration() {
-
-    private val left = if (isHorizontal) null else paddings.left.roundToInt()
-    private val top = if (isHorizontal) paddings.top.roundToInt() else null
-    private val right = if (isHorizontal) null else paddings.right.roundToInt()
-    private val bottom = if (isHorizontal) paddings.bottom.roundToInt() else null
 
     override fun getItemOffsets(outRect: Rect, view: View, parent: RecyclerView, state: RecyclerView.State) {
         view.measure(makeUnspecifiedSpec(), makeUnspecifiedSpec())
         val child = (view as? DivPagerPageLayout)?.child ?: return
         outRect.set(
-            left ?: ((parentSize - child.measuredWidth) / 2f).roundToInt(),
-            top ?: ((parentSize - child.measuredHeight) / 2f).roundToInt(),
-            right ?: ((parentSize - child.measuredWidth) / 2f).roundToInt(),
-            bottom ?: ((parentSize - child.measuredHeight) / 2f).roundToInt()
+            paddings.alignedLeft ?: child.horizontalOffset,
+            paddings.alignedTop ?: child.topOffset,
+            paddings.alignedRight ?: child.horizontalOffset,
+            paddings.alignedBottom ?: child.bottomOffset
         )
+    }
+
+    private val View.horizontalOffset: Int get() {
+        return when (alignment) {
+            ItemAlignment.START -> parentSize - paddings.start - measuredWidth
+            ItemAlignment.CENTER -> (parentSize - measuredWidth) / 2f
+            ItemAlignment.END -> parentSize - paddings.end - measuredWidth
+        }.roundToInt()
+    }
+
+    private val View.topOffset: Int get() {
+        return when (alignment) {
+            ItemAlignment.START -> paddings.start
+            ItemAlignment.CENTER -> (parentSize - measuredHeight) / 2f
+            ItemAlignment.END -> parentSize - paddings.end - measuredHeight
+        }.roundToInt()
+    }
+
+    private val View.bottomOffset: Int get() {
+        return when (alignment) {
+            ItemAlignment.START -> parentSize - paddings.start - measuredHeight
+            ItemAlignment.CENTER -> (parentSize - measuredHeight) / 2f
+            ItemAlignment.END -> paddings.end
+        }.roundToInt()
     }
 }
