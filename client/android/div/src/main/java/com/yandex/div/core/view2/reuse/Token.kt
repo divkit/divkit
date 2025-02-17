c9180e612a65f64a9ecbc13ae39a7582ec33b33b
pkurchatov <pkurchatov@yandex-team.com>
2025-02-17 11:12:41 +0300

    Added type check in complex rebind commit_hash:40a50b2f6319870e706d393e6c962b2763b316f8

--- client/android/div/src/main/java/com/yandex/div/core/view2/reuse/Token.kt
+++ client/android/div/src/main/java/com/yandex/div/core/view2/reuse/Token.kt
@@ -1,5 +1,6 @@
 package com.yandex.div.core.view2.reuse
 
+import com.yandex.div.core.util.type
 import com.yandex.div.internal.core.DivItemBuilderResult
 
 internal abstract class Token(
@@ -8,4 +9,8 @@ internal abstract class Token(
 ) {
     val divHash: Int = item.div.propertiesHash()
     val div = item.div
+
+    fun isCombinable(other: Token): Boolean {
+        return divHash == other.divHash && div.type == other.div.type
+    }
 }
