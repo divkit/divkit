2b78ecea465bfe64f1a542113790b3f93579ab1e
booster <booster@yandex-team.com>
2025-03-24 13:45:23 +0300

    Fixed indicator animation commit_hash:4a8db046bdecebb67f3f74359129bf7ac8d2bd99

--- client/ios/LayoutKit/LayoutKit/UI/Base/PageIndicator/PageIndicatorLayer.swift
+++ client/ios/LayoutKit/LayoutKit/UI/Base/PageIndicator/PageIndicatorLayer.swift
@@ -84,7 +84,6 @@ final class ScrollPageIndicatorLayer: CALayer {
 
   override func action(forKey key: String) -> CAAction? {
     guard Self.isAnimationKeySupported(key),
-          animation(forKey: key) == nil,
           let presentation = presentation(),
           let fromValue = presentation.value(forKey: key) else {
       return super.action(forKey: key)
@@ -229,25 +228,29 @@ extension ScrollPageIndicatorLayer {
     func run(forKey event: String, object anObject: Any, arguments _: [AnyHashable: Any]?) {
       guard let layer = anObject as? CALayer,
             let fromValue = self.fromValue as? Double,
-            let toValue = layer.value(forKey: event) as? Int,
-            toValue != numberOfPages else {
+            let currentIndex = (layer.value(forKey: event) as? Int),
+            currentIndex != numberOfPages else {
         return
       }
 
-      let lastPage = numberOfPages - 1
-      let correctedFromValue = if fromValue.isApproximatelyLessOrEqualThan(0), toValue == lastPage {
-        Double(numberOfPages)
-      } else if fromValue.isApproximatelyGreaterOrEqualThan(Double(lastPage)), toValue == 0 {
-        Double(-1)
-      } else {
-        fromValue
+      let addAnimation = { fromValue in
+        let animation = CABasicAnimation(keyPath: event)
+        animation.fromValue = fromValue
+        layer.addAnimation(animation)
       }
 
-      let animation = CABasicAnimation(keyPath: event)
-      animation.fromValue = correctedFromValue
-      animation.toValue = toValue
-
-      layer.add(animation, forKey: event)
+      let fromIndex = Int(fromValue)
+      let indexDifference = currentIndex - fromIndex
+      let numberOfPages = Double(numberOfPages)
+      let correctedFromValue: Double = switch indexDifference {
+      case ...(-2): // From last to first element
+        -1
+      case 2...: // From first to last element
+        numberOfPages
+      default:
+        fromValue
+      }
+      addAnimation(correctedFromValue)
     }
   }
 }
