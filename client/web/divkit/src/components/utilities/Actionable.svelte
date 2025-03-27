3cd5caf21c058836349040678e70622eebd7c073
4eb0da <4eb0da@yandex-team.com>
2025-03-27 12:34:52 +0300

    Upd nested actions logic commit_hash:69f322ade12692071dfb79e228c71b7d6bbae314

--- client/web/divkit/src/components/utilities/Actionable.svelte
+++ client/web/divkit/src/components/utilities/Actionable.svelte
@@ -82,7 +82,8 @@
             }
         }
 
-        if (Array.isArray(actions) && actions?.length && actionCtx.hasAction()) {
+        hasJSAction = Boolean(customAction);
+        if ((href || Array.isArray(actions) && actions?.length) && actionCtx.hasAction()) {
             href = '';
             componentContext.logError(wrapError(new Error('Actionable element is forbidden inside other actionable element'), {
                 level: 'warn',
@@ -90,10 +91,7 @@
                     actions
                 }
             }));
-        }
-
-        hasJSAction = Boolean(customAction);
-        if (href && !isBuiltinSchema(getUrlSchema(href), rootCtx.getBuiltinProtocols())) {
+        } else if (href && !isBuiltinSchema(getUrlSchema(href), rootCtx.getBuiltinProtocols())) {
             href = '';
             hasJSAction = true;
         } else if (!href && Array.isArray(actions) && actions?.length) {
