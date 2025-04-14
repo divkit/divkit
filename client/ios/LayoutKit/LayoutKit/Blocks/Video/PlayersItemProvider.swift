6d2f2102d01fe31353784f8da9cb890d0388979a
denlvovich <denlvovich@yandex-team.com>
2025-04-14 13:06:12 +0300

    Fixed Video callbacks and Video stream regression tests in playground commit_hash:f3e613e9ea2710b56e52af6a5225ff9ba9da5289

--- client/ios/LayoutKit/LayoutKit/Blocks/Video/PlayersItemProvider.swift
+++ client/ios/LayoutKit/LayoutKit/Blocks/Video/PlayersItemProvider.swift
@@ -16,12 +16,20 @@ public class DefaultPlayerItemsProvider: PlayerItemsProvider {
 
 extension CachedURLResourceRequester: PlayerItemsProvider {
   public func getAVPlayerItem(from url: URL, completion: @escaping (AVPlayerItem) -> Void) {
+    let streamingVideoExtensions = ["m3u8", "m3u"]
+    guard !streamingVideoExtensions.contains(url.pathExtension) else {
+      completion(AVPlayerItem(url: url)) // Cannot cache "m3u8", load online
+      return
+    }
+
     if let localURL = self.getLocalResourceURL(with: url) {
       completion(AVPlayerItem(url: localURL))
     } else {
       let _ = self.getDataWithSource(from: url) { [weak self] _ in
         if let localURL = self?.getLocalResourceURL(with: url) {
           completion(AVPlayerItem(url: localURL))
+        } else {
+          completion(AVPlayerItem(url: url)) // Unable to cache file, invalid URL assumed
         }
       }
     }
