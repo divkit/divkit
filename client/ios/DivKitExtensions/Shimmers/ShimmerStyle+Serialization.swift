e2599b1286a90bd0ef2aba4a25b076776bcaad8a
feldspar <feldspar@yandex-team.com>
2025-04-14 11:41:10 +0300

    Moved dictionary extension to separate file commit_hash:06c8713c8be2e7bb8139866017c7042099b7731e

--- client/ios/DivKitExtensions/Shimmers/ShimmerStyle+Serialization.swift
+++ client/ios/DivKitExtensions/Shimmers/ShimmerStyle+Serialization.swift
@@ -41,38 +41,6 @@ extension DivBase {
 }
 
 extension Dictionary where Key == String {
-  func getOptionalBool(
-    _ key: Key,
-    expressionResolver: ExpressionResolver
-  ) throws -> Bool? {
-    let result: Bool?
-    if let value = self[key] as? Bool {
-      result = value
-    } else if let expression: String = try getOptionalField(key) {
-      result = expressionResolver.resolve(expression) as? Bool
-    } else {
-      return nil
-    }
-    return result
-  }
-
-  func getOptionalFloat(
-    _ key: Key,
-    expressionResolver: ExpressionResolver
-  ) throws -> CGFloat? {
-    let result: Double?
-    if let value = self[key] as? Double {
-      result = value
-    } else if let value = self[key] as? Int {
-      result = Double(value)
-    } else if let expression: String = try getOptionalField(key) {
-      result = expressionResolver.resolveNumeric(expression)
-    } else {
-      return nil
-    }
-    return result.map { CGFloat($0) }
-  }
-
   func getOptionalColorArray(
     _ key: Key,
     expressionResolver: ExpressionResolver
