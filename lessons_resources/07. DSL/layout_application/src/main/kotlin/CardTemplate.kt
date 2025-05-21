daf64fa05b6c9c2b4654f42d67813b8a0e6e4fb0
the-leo <the-leo@yandex-team.com>
2025-05-21 12:53:35 +0300

    List of biv binding example commit_hash:85b7db0a8b16f1f7f259212e435791744dfd629b

--- lessons_resources/07. DSL/layout_application/src/main/kotlin/CardTemplate.kt
+++ lessons_resources/07. DSL/layout_application/src/main/kotlin/CardTemplate.kt
@@ -1,6 +1,7 @@
 package com.yandex.divkit.example.dsl
 
 import com.yandex.divkit.example.dsl.data.Variables
+import divkit.dsl.Div
 import divkit.dsl.Url
 import divkit.dsl.action
 import divkit.dsl.actionSetVariable
@@ -10,7 +11,11 @@ import divkit.dsl.core.reference
 import divkit.dsl.defer
 import divkit.dsl.edgeInsets
 import divkit.dsl.fixedSize
+import divkit.dsl.horizontal
 import divkit.dsl.image
+import divkit.dsl.matchParentSize
+import divkit.dsl.separator
+import divkit.dsl.space_around
 import divkit.dsl.stringValue
 import divkit.dsl.template
 import divkit.dsl.text
@@ -18,6 +23,7 @@ import divkit.dsl.text
 object CardTemplate {
     val descriptionRef = reference<String>("description")
     val imageRef = reference<Url>("img")
+    val buttonsRef = reference<List<Div>>("buttons")
 
     val template by lazy(LazyThreadSafetyMode.NONE) {
         template("sample_card") {
@@ -27,26 +33,52 @@ object CardTemplate {
                     value = 150,
                 ),
                 items = listOf(
-                    image().defer(
-                        imageUrl = imageRef,
+                    container(
+                        items = listOf(
+                            image().defer(
+                                imageUrl = imageRef,
+                            ),
+                            text(
+                                textAlignmentHorizontal = center,
+                            ).defer(
+                                text = descriptionRef,
+                            ),
+                        ),
+                        actions = listOf(
+                            action(
+                                logId = "post_selected",
+                                typed = actionSetVariable(
+                                    variableName = Variables.SELECTED_POST_DESCRIPTION,
+                                    value = stringValue().defer(
+                                        value = descriptionRef,
+                                    )
+                                )
+                            )
+                        ),
+                    ),
+                    separator(
+                        width = matchParentSize(),
+                        height = fixedSize(1),
+                        margins = edgeInsets(
+                            top = 1,
+                            bottom = 4,
+                        ),
                     ),
                     text(
+                        text = "Share",
                         textAlignmentHorizontal = center,
+                        margins = edgeInsets(
+                            bottom = 4,
+                        ),
+                    ),
+                    container(
+                        orientation = horizontal,
+                        width = matchParentSize(),
+                        contentAlignmentHorizontal = space_around,
                     ).defer(
-                        text = descriptionRef,
+                        items = buttonsRef,
                     )
                 ),
-                actions = listOf(
-                    action(
-                        logId = "post_selected",
-                        typed = actionSetVariable(
-                            variableName = Variables.SELECTED_POST_DESCRIPTION,
-                            value = stringValue().defer(
-                                value = descriptionRef,
-                            )
-                        )
-                    )
-                )
             )
         }
     }
--- lessons_resources/07. DSL/layout_application/src/main/kotlin/LayoutRenderer.kt
+++ lessons_resources/07. DSL/layout_application/src/main/kotlin/LayoutRenderer.kt
@@ -1,11 +1,15 @@
 package com.yandex.divkit.example.dsl
 
 import com.yandex.divkit.example.dsl.data.PostData
+import com.yandex.divkit.example.dsl.data.Preview
 import com.yandex.divkit.example.dsl.data.Variables.SELECTED_POST_DESCRIPTION
 import com.yandex.divkit.example.dsl.data.exampleData
 import divkit.dsl.Div
 import divkit.dsl.Divan
+import divkit.dsl.Image
 import divkit.dsl.Url
+import divkit.dsl.accessibility
+import divkit.dsl.action
 import divkit.dsl.center
 import divkit.dsl.container
 import divkit.dsl.core.bind
@@ -13,13 +17,16 @@ import divkit.dsl.core.expression
 import divkit.dsl.data
 import divkit.dsl.divan
 import divkit.dsl.evaluate
+import divkit.dsl.fixedSize
 import divkit.dsl.gallery
 import divkit.dsl.horizontal
+import divkit.dsl.image
 import divkit.dsl.render
 import divkit.dsl.root
 import divkit.dsl.scope.DivScope
 import divkit.dsl.stringVariable
 import divkit.dsl.text
+import java.net.URL
 
 class LayoutRenderer {
     fun render(): Divan {
@@ -63,12 +70,40 @@ class LayoutRenderer {
         )
     }
 
-    private fun DivScope.createCard(person: PostData): Div {
+    private fun DivScope.createSocialMediaButton(
+        postImageUrl: URL,
+        mediaName: String,
+        preview: String,
+    ): Image = image(
+        imageUrl = Url.create("empty://"),
+        preview = preview,
+        width = fixedSize(25),
+        height = fixedSize(25),
+        accessibility = accessibility(
+            description = mediaName,
+        ),
+        actions = listOf(
+            action(
+                logId = "shared to $mediaName",
+                url = Url.create("my_client_action_handler://url=$postImageUrl&media=$mediaName"),
+            )
+        ),
+    )
+
+    private fun DivScope.createSocialMediaButtons(postImageUrl: URL): List<Div> = listOf(
+        createSocialMediaButton(postImageUrl, "Div 1 Media", Preview.MEDIA_DIV_1),
+        createSocialMediaButton(postImageUrl, "Div 2 Media", Preview.MEDIA_DIV_2),
+        createSocialMediaButton(postImageUrl, "Div 3 Media", Preview.MEDIA_DIV_3),
+        createSocialMediaButton(postImageUrl, "Div 4 Media", Preview.MEDIA_DIV_4),
+    )
+
+    private fun DivScope.createCard(post: PostData): Div {
         return with(CardTemplate) {
             render(
                 template,
-                descriptionRef bind person.description,
-                imageRef bind Url.create(person.imageUrl.toString()),
+                descriptionRef bind post.description,
+                imageRef bind Url.create(post.imageUrl.toString()),
+                buttonsRef bind createSocialMediaButtons(post.imageUrl),
             )
         }
     }
