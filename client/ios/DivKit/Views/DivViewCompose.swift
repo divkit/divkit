3781c4f5bf5a4c6a044f2fdcb33ea4477bdecdc8
burstein <burstein@yandex-team.com>
2025-07-07 01:34:31 +0300

    Added DivKitView objc factory commit_hash:43db8077e82696290c6a92d30589c3106dd24389

---
+++ client/ios/DivKit/Views/DivViewCompose.swift
@@ -0,0 +1,11 @@
+import UIKit
+
+@objc(ComposeDivKitFactory) public class ComposeDivKitFactory: NSObject {
+  @objc public static func makeDivKitView(_ jsonString: String) -> UIView {
+    let view = DivView(divKitComponents: DivKitComponents())
+    Task {
+      await view.setSource(DivViewSource(kind: .data(Data(jsonString.utf8)), cardId: "Sample"))
+    }
+    return view
+  }
+}
