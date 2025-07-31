836ebbf4032c2f6a102c1afc33842a87c813531b
bacecek <bacecek@yandex-team.com>
2025-07-31 15:16:33 +0300

    Enable configuration cache by default commit_hash:26a898b2d11e9204c99540beb0741917610ab09d

--- client/android/buildSrc/src/main/java/com/yandex/div/gradle/PublicationType.kt
+++ client/android/buildSrc/src/main/java/com/yandex/div/gradle/PublicationType.kt
@@ -1,5 +1,7 @@
 package com.yandex.div.gradle
 
+import org.gradle.api.Project
+import org.gradle.api.publish.maven.tasks.AbstractPublishToMaven
 import java.text.SimpleDateFormat
 import java.util.Date
 
@@ -9,13 +11,22 @@ enum class PublicationType {
 
     dev {
         override fun getVersionSuffix() = "-dev.${VERSION_DATE_FORMAT.format(Date())}"
+        override fun configureForProject(project: Project) {
+            project.tasks.withType(AbstractPublishToMaven::class.java).configureEach { task ->
+                task.notCompatibleWithConfigurationCache("publicationType dev is not compatible " +
+                        "with configuration cache as it uses build start timestamp")
+            }
+        }
     },
 
     release {
         override fun getVersionSuffix() = ""
+        override fun configureForProject(project: Project) = Unit
     };
 
     abstract fun getVersionSuffix(): String
+
+    abstract fun configureForProject(project: Project)
 
     companion object {
 
