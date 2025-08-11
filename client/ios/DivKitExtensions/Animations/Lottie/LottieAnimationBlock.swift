9e54b073bfe01cdc55191c4e3d48be7e917e93f5
denlvovich <denlvovich@yandex-team.com>
2025-08-11 16:08:21 +0300

    Fixed calculation of lottie block sizes with fixed values commit_hash:0288ebadb407196c4db9e5f5011a401a049bd54a

--- client/ios/DivKitExtensions/Animations/Lottie/LottieAnimationBlock.swift
+++ client/ios/DivKitExtensions/Animations/Lottie/LottieAnimationBlock.swift
@@ -11,8 +11,6 @@ final class LottieAnimationBlock: SizeForwardingBlock {
   let scale: DivImageScale
   var isPlaying: Bool
 
-  let intrinsicContentWidth: CGFloat = 0
-
   var debugDescription: String {
     "Animation Block playing animation with view: \(animatableView)"
   }
