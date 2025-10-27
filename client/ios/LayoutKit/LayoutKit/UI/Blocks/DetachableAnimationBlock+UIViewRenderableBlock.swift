5d5cf60212df055d9ab8f507d667f36274561c4e
feldspar <feldspar@yandex-team.com>
2025-10-27 21:08:36 +0300

    Fixed hit test for DetachableAnimationBlockView commit_hash:4596498ee1e11f7a46ac565a3480e274210fd291

--- client/ios/LayoutKit/LayoutKit/UI/Blocks/DetachableAnimationBlock+UIViewRenderableBlock.swift
+++ client/ios/LayoutKit/LayoutKit/UI/Blocks/DetachableAnimationBlock+UIViewRenderableBlock.swift
@@ -92,6 +92,11 @@ final class DetachableAnimationBlockView: BlockView, DelayedVisibilityActionView
     }
   }
 
+  override func hitTest(_ point: CGPoint, with event: UIEvent?) -> UIView? {
+    let result = super.hitTest(point, with: event)
+    return result === self ? nil : result
+  }
+
   func convertFrame(to container: UIView) -> CGRect {
     convert(bounds, to: container)
   }
