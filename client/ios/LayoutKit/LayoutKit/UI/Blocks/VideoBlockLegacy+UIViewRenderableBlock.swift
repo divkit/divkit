2c2b394dbde86af0598caec41a081414549c90c9
booster <booster@yandex-team.com>
2025-11-19 12:16:11 +0300

    Simplified VideoBlockLegacy looping commit_hash:04b28c6139b950a9b057a40d64798d5bff9d8853

--- client/ios/LayoutKit/LayoutKit/UI/Blocks/VideoBlockLegacy+UIViewRenderableBlock.swift
+++ client/ios/LayoutKit/LayoutKit/UI/Blocks/VideoBlockLegacy+UIViewRenderableBlock.swift
@@ -20,7 +20,7 @@ extension VideoBlockLegacy {
     renderingDelegate _: RenderingDelegate?
   ) {
     let videoView = view as! VideoBlockLegacyView
-    videoView.autoplayAllowed.source = autoplayAllowed.newValues
+    videoView.autoplayAllowed.source = autoplayAllowed.currentAndNewValues
     if videoView.videoAssetHolder?.url != videoAssetHolder.url {
       videoView.videoAssetHolder = videoAssetHolder
     }
@@ -46,21 +46,26 @@ private final class VideoBlockLegacyView: BlockView {
   let effectiveBackgroundColor: UIColor? = nil
 
   private let disposePool = AutodisposePool()
-  private let avPlayer = AVQueuePlayer()
-  private var playerLooper: AVPlayerLooper?
+  private let avPlayer = AVPlayer()
 
   private var playerLayer: AVPlayerLayer { layer as! AVPlayerLayer }
 
   init() {
     autoplayAllowed = .init(initialValue: false)
     super.init(frame: .zero)
-    autoplayAllowed.target.newValues.addObserver { _ in
+    autoplayAllowed.target.newValues.skipRepeats().addObserver { _ in
       self.resumePlayer()
     }.dispose(in: disposePool)
     NotificationCenter.default.addObserver(
       self,
       selector: #selector(resumePlayer),
       name: UIApplication.willEnterForegroundNotification,
+      object: nil
+    )
+    NotificationCenter.default.addObserver(
+      self,
+      selector: #selector(playerItemDidReachEnd),
+      name: AVPlayerItem.didPlayToEndTimeNotification,
       object: nil
     )
     playerLayer.player = avPlayer
@@ -79,14 +84,20 @@ private final class VideoBlockLegacyView: BlockView {
   func onVisibleBoundsChanged(from _: CGRect, to _: CGRect) {}
 
   private func configurePlayer(with playerItem: AVPlayerItem) {
-    avPlayer.removeAllItems()
-    playerLooper = AVPlayerLooper(player: avPlayer, templateItem: playerItem)
+    avPlayer.replaceCurrentItem(with: playerItem)
     resumePlayer()
   }
 
   @objc private func resumePlayer() {
-    if autoplayAllowed.value, playerLooper != nil {
+    if autoplayAllowed.value {
       avPlayer.play()
+    }
+  }
+
+  @objc private func playerItemDidReachEnd(notification: Notification) {
+    if (notification.object as? AVPlayerItem) === avPlayer.currentItem {
+      avPlayer.seek(to: .zero)
+      resumePlayer()
     }
   }
 }
