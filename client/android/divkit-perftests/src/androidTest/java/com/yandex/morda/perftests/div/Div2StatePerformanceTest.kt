72f8bfa1b097aa7784f7b26822318f3330431674
i-ts <i-ts@yandex-team.com>
2025-12-24 12:09:30 +0300

    increase binding duration and enable Div.Binding.Cold tracking commit_hash:88546e812634fd822a675b33caea54c437d1f615

--- client/android/divkit-perftests/src/androidTest/java/com/yandex/morda/perftests/div/Div2StatePerformanceTest.kt
+++ client/android/divkit-perftests/src/androidTest/java/com/yandex/morda/perftests/div/Div2StatePerformanceTest.kt
@@ -21,7 +21,7 @@ import org.junit.Test
 class Div2StatePerformanceTest : Div2BasePerformanceTest() {
     @PerfTestParameter(
         importantMetrics = [
-            //"Div.Binding.Cold", // Generates unstable results at some perf-tests.
+            "Div.Binding.Cold",
             "Div.Parsing.Data.Cold",
             //"Div.Parsing.JSON.Cold", // Generates unstable results at some perf-tests.
             "Div.Parsing.Templates.Cold",
