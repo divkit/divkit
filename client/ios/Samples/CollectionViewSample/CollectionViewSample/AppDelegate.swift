3739f4d359633a6cd2604d59e795c443cf60d586
booster <booster@yandex-team.com>
2026-02-19 13:11:20 +0300

    Added collection view ios sample commit_hash:0f36266bb814aec8babef2f9ddb2f9333dbd2f1e

---
+++ client/ios/Samples/CollectionViewSample/CollectionViewSample/AppDelegate.swift
@@ -0,0 +1,13 @@
+import UIKit
+
+@main
+final class AppDelegate: UIResponder, UIApplicationDelegate {
+  var window: UIWindow?
+
+  func application(
+    _: UIApplication,
+    didFinishLaunchingWithOptions _: [UIApplication.LaunchOptionsKey: Any]?
+  ) -> Bool {
+    true
+  }
+}
