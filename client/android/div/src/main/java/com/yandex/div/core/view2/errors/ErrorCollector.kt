61901af4cb39dd91ea3362c357bb8e93774851bf
gulevsky <gulevsky@yandex-team.com>
2026-02-27 11:01:24 +0300

    fix ConcurrentModificationException at ErrorCollector.notifyObservers() call commit_hash:adb427412702395c513c27f1d8c989359e9750c8

--- client/android/div/src/main/java/com/yandex/div/core/view2/errors/ErrorCollector.kt
+++ client/android/div/src/main/java/com/yandex/div/core/view2/errors/ErrorCollector.kt
@@ -1,21 +1,25 @@
 package com.yandex.div.core.view2.errors
 
+import androidx.annotation.AnyThread
 import com.yandex.div.DivDataTag
 import com.yandex.div.core.Disposable
 import com.yandex.div.core.DivErrorsReporter
 import com.yandex.div.core.annotations.Mockable
 import com.yandex.div.json.ParsingErrorLogger
 import com.yandex.div2.DivData
-import java.lang.Exception
 
 typealias ErrorObserver = (errors: List<Throwable>, warnings: List<Throwable>) -> Unit
 
 @Mockable
+@AnyThread
 internal class ErrorCollector(
     internal val divData: DivData?,
     private val divDataTag: DivDataTag,
     private val divErrorsReporter: DivErrorsReporter,
 ) : ParsingErrorLogger {
+
+    private val mutex = Any()
+
     private val observers = mutableSetOf<ErrorObserver>()
     private val runtimeErrors = mutableListOf<Throwable>()
     private var parsingErrors = emptyList<Throwable>()
@@ -24,7 +28,7 @@ internal class ErrorCollector(
     private var errors = mutableListOf<Throwable>()
     private var errorsAreValid = true
 
-    fun logError(e: Throwable) {
+    fun logError(e: Throwable): Unit = synchronized(mutex) {
         runtimeErrors.add(e)
         divErrorsReporter.onRuntimeError(divData, divDataTag, e)
         notifyObservers()
@@ -32,21 +36,21 @@ internal class ErrorCollector(
 
     override fun logError(e: Exception) = logError(e as Throwable)
 
-    fun logWarning(warning: Throwable) {
+    fun logWarning(warning: Throwable): Unit = synchronized(mutex) {
         warnings.add(warning)
         divErrorsReporter.onWarning(divData, divDataTag, warning)
         notifyObservers()
     }
 
-    fun getWarnings(): Iterator<Throwable> = warnings.listIterator()
+    fun getWarnings(): Iterable<Throwable> = synchronized(mutex) { warnings.toList() }
 
-    fun cleanRuntimeWarningsAndErrors() {
+    fun cleanRuntimeWarningsAndErrors(): Unit = synchronized(mutex) {
         warnings.clear()
         runtimeErrors.clear()
         notifyObservers()
     }
 
-    private fun notifyObservers() {
+    private fun notifyObservers(): Unit = synchronized(mutex) {
         errorsAreValid = false
         if (observers.isEmpty()) return
         rebuildErrors()
@@ -61,14 +65,14 @@ internal class ErrorCollector(
         errorsAreValid = true
     }
 
-    fun observeAndGet(observer: ErrorObserver): Disposable {
+    fun observeAndGet(observer: ErrorObserver): Disposable = synchronized(mutex)  {
         observers.add(observer)
         rebuildErrors()
         observer(errors, warnings)
         return Disposable { observers.remove(observer) }
     }
 
-    fun attachParsingErrors() {
+    fun attachParsingErrors(): Unit = synchronized(mutex)  {
         parsingErrors = divData?.parsingErrors ?: emptyList()
         notifyObservers()
     }
