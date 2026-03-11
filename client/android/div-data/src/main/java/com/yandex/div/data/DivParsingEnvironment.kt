5a258fa1e315b69c8730a8912641062d93329a50
pkurchatov <pkurchatov@yandex-team.com>
2026-03-11 12:03:00 +0300

    Removed unused classes commit_hash:d93bb853c44d8a8380ae96a84d0bb77cb43e3a70

--- client/android/div-data/src/main/java/com/yandex/div/data/DivParsingEnvironment.kt
+++ client/android/div-data/src/main/java/com/yandex/div/data/DivParsingEnvironment.kt
@@ -3,7 +3,6 @@ package com.yandex.div.data
 import com.yandex.div.core.annotations.Mockable
 import com.yandex.div.json.ParsingErrorLogger
 import com.yandex.div.json.TemplateParsingEnvironment
-import com.yandex.div.json.TemplateParsingEnvironment.TemplateFactory
 import com.yandex.div.json.templates.CachingTemplateProvider
 import com.yandex.div.json.templates.InMemoryTemplateProvider
 import com.yandex.div.json.templates.TemplateProvider
