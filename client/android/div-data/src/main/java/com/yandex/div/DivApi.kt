bf12606222e93736c8b3b4fe52a05cfabdd37f1c
grechka62 <grechka62@yandex-team.com>
2026-03-16 13:24:50 +0300

    Revert DivApi removing commit_hash:4631de992bb933fe4b1d6d4e59b88fa44818e514

---
+++ client/android/div-data/src/main/java/com/yandex/div/DivApi.kt
@@ -0,0 +1,14 @@
+package com.yandex.div
+
+
+/**
+ * Div API parameters.
+ */
+@Deprecated("Will be removed")
+object DivApi {
+
+    /**
+     * Div API version.
+     */
+    const val VERSION = "2.3"
+}
