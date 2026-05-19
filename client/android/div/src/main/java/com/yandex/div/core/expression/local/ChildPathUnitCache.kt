ee85e79393b7c7a246f5d4a74659c50da12db74f
booster <booster@yandex-team.com>
2026-05-19 10:50:44 +0300

    Fixed race conditions in preload commit_hash:a9f53a9f57bd738ac1c68a390a9a3519e15e91cc

--- client/android/div/src/main/java/com/yandex/div/core/expression/local/ChildPathUnitCache.kt
+++ client/android/div/src/main/java/com/yandex/div/core/expression/local/ChildPathUnitCache.kt
@@ -1,19 +1,13 @@
 package com.yandex.div.core.expression.local
 
 internal object ChildPathUnitCache {
-    private var cache = ArrayList<String>()
+    private val cache = ArrayList<String>()
 
-    private fun ensureGenerated(index: Int) {
-        if (cache.size > index) return
-
-        cache.ensureCapacity(index + 1)
-        for (i in cache.size..index) {
-            cache.add(i, "child#$i")
-        }
-    }
-
+    @Synchronized
     internal fun getValue(index: Int): String {
-        ensureGenerated(index)
+        while (cache.size <= index) {
+            cache.add("child#${cache.size}")
+        }
         return cache[index]
     }
 }
