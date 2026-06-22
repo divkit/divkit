abbdfa5f874b5d16780ea062e262cb57927ae807
4eb0da <4eb0da@yandex-team.com>
2026-06-22 11:24:19 +0300

    Fix site templates commit_hash:c32c294d19d006cf1bc3a11b7b2644733aa373e4

--- site/client/src/data/defaultEditorValue.ts
+++ site/client/src/data/defaultEditorValue.ts
@@ -39,35 +39,6 @@ export const DEFAULT_EDITOR_VALUE = `{
     ]
   },
   "templates": {
-    "_template_lottie": {
-      "type": "gif",
-      "scale": "fit",
-      "extensions": [
-        {
-          "id": "lottie",
-          "params": {
-            "$lottie_url": "lottie_url"
-          }
-        }
-      ],
-      "gif_url": "https://empty"
-    },
-    "_template_button": {
-      "type": "text",
-      "content_alignment_horizontal": "center",
-      "border": {
-        "$corner_radius": "corners"
-      },
-      "paddings": {
-        "bottom": 24,
-        "left": 28,
-        "right": 28,
-        "top": 22
-      },
-      "width": {
-        "type": "wrap_content"
-      }
-    },
     "_template_close": {
       "accessibility": {
         "description": "Закрыть",
@@ -83,44 +54,6 @@ export const DEFAULT_EDITOR_VALUE = `{
       "image_url": "https://yastatic.net/s3/home/div/div_fullscreens/cross2.3.png",
       "tint_color": "#73000000",
       "type": "image"
-    },
-    "_template_list_item": {
-      "type": "container",
-      "orientation": "horizontal",
-      "items": [
-        {
-          "type": "image",
-          "image_url": "https://yastatic.net/s3/home/div/div_fullscreens/hyphen.4.png",
-          "$tint_color": "list_color",
-          "width": {
-            "type": "fixed",
-            "value": 28,
-            "unit": "sp"
-          },
-          "height": {
-            "type": "fixed",
-            "value": 28,
-            "unit": "sp"
-          },
-          "margins": {
-            "top": 2,
-            "right": 12,
-            "bottom": 2
-          }
-        },
-        {
-          "type": "text",
-          "$text": "list_text",
-          "$text_color": "list_color",
-          "font_size": 24,
-          "line_height": 32,
-          "font_weight": "medium",
-          "width": {
-            "type": "wrap_content",
-            "constrained": true
-          }
-        }
-      ]
     }
   }
 }`;
