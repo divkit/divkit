c0f2def570fc63338866437e3607919567fa379a
pkurchatov <pkurchatov@yandex-team.com>
2026-07-21 12:54:05 +0300

    Optimzed container separators rendering commit_hash:b32aa926d0ba8dd1bcb7e44d4cefee4a8feb2b5b

--- client/android/compose/src/main/kotlin/com/yandex/div/compose/views/container/ContainerSeparator.kt
+++ client/android/compose/src/main/kotlin/com/yandex/div/compose/views/container/ContainerSeparator.kt
@@ -1,16 +1,16 @@
 package com.yandex.div.compose.views.container
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.drawBehind
-import androidx.compose.ui.geometry.CornerRadius
 import com.yandex.div.compose.expressions.observedColorValue
-import com.yandex.div.compose.expressions.observedValue
-import com.yandex.div.compose.utils.toDp
+import com.yandex.div.compose.utils.observedValue
 import com.yandex.div.compose.views.modifiers.padding
 import com.yandex.div2.DivContainer
 import com.yandex.div2.DivDrawable
@@ -19,40 +19,34 @@ import com.yandex.div2.DivShape
 @Composable
 internal fun ContainerSeparator(
     separator: DivContainer.Separator,
-    modifier: Modifier = Modifier,
+    modifier: Modifier = Modifier
 ) {
-    val style = separator.style as? DivDrawable.Shape ?: return
-    val shapeDrawable = style.value
-    val color = shapeDrawable.color.observedColorValue()
+    when (val style = separator.style) {
+        is DivDrawable.Shape -> {
+            val color = style.value.color.observedColorValue()
+            val modifier = modifier.padding(separator.margins)
+            when (val shape = style.value.shape) {
+                is DivShape.RoundedRectangle -> {
+                    val rect = shape.value
+                    Spacer(
+                        modifier = modifier
+                            .width(rect.itemWidth.observedValue())
+                            .height(rect.itemHeight.observedValue())
+                            .background(
+                                color = color,
+                                shape = RoundedCornerShape(rect.cornerRadius.observedValue())
+                            )
+                    )
+                }
 
-    val modifier = modifier.padding(separator.margins)
-
-    when (val shape = shapeDrawable.shape) {
-        is DivShape.RoundedRectangle -> {
-            val rect = shape.value
-            val cornerRadiusDp = rect.cornerRadius.value.observedValue().toDp()
-            Spacer(
-                modifier = modifier
-                    .width(rect.itemWidth.value.observedValue().toDp())
-                    .height(rect.itemHeight.value.observedValue().toDp())
-                    .drawBehind {
-                        drawRoundRect(
-                            color = color,
-                            cornerRadius = CornerRadius(cornerRadiusDp.toPx()),
-                        )
-                    }
-            )
-        }
-
-        is DivShape.Circle -> {
-            val circle = shape.value
-            val radius = circle.radius.value.observedValue().toDp()
-            val diameter = radius * 2
-            Spacer(
-                modifier = modifier
-                    .size(diameter)
-                    .drawBehind { drawCircle(color = color) }
-            )
+                is DivShape.Circle -> {
+                    Spacer(
+                        modifier = modifier
+                            .size(shape.value.radius.observedValue() * 2)
+                            .background(color, CircleShape)
+                    )
+                }
+            }
         }
     }
 }
