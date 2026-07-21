3f6edc051a95774c15910f8ec10b303cf2cfc2bd
pkurchatov <pkurchatov@yandex-team.com>
2026-07-21 10:26:50 +0300

    ExpressionListUtils optimization commit_hash:1365bd1fa36a182e3a0098041a2ed99def2ea8ee

--- client/android/compose/src/main/kotlin/com/yandex/div/compose/expressions/ExpressionListUtils.kt
+++ client/android/compose/src/main/kotlin/com/yandex/div/compose/expressions/ExpressionListUtils.kt
@@ -1,12 +1,14 @@
 package com.yandex.div.compose.expressions
 
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.RememberObserver
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import com.yandex.div.compose.context.expressionResolver
+import com.yandex.div.core.Disposable
 import com.yandex.div.json.expressions.ConstantExpressionList
 import com.yandex.div.json.expressions.ExpressionList
+import com.yandex.div.json.expressions.ExpressionResolver
 
 @Composable
 internal fun <T : Any> ExpressionList<T>.observedValue(): List<T> {
@@ -14,16 +16,35 @@ internal fun <T : Any> ExpressionList<T>.observedValue(): List<T> {
     return when (this) {
         is ConstantExpressionList -> evaluate(expressionResolver)
         else -> {
-            val state = remember(this, expressionResolver) {
-                mutableStateOf(evaluate(expressionResolver))
+            val observer = remember(this, expressionResolver) {
+                ExpressionListObserver(
+                    expressionList = this,
+                    expressionResolver = expressionResolver
+                )
             }
-
-            DisposableEffect(this, expressionResolver) {
-                val disposable = observe(expressionResolver) { state.value = it }
-                onDispose { disposable.close() }
-            }
-
-            state.value
+            observer.value
         }
     }
+}
+
+private class ExpressionListObserver<T : Any>(
+    private val expressionList: ExpressionList<T>,
+    private val expressionResolver: ExpressionResolver
+) : RememberObserver {
+    private val state = mutableStateOf(expressionList.evaluate(expressionResolver))
+
+    private var subscription: Disposable? = null
+
+    val value: List<T> get() = state.value
+
+    override fun onRemembered() {
+        subscription = expressionList.observe(expressionResolver) { state.value = it }
+    }
+
+    override fun onForgotten() {
+        subscription?.close()
+        subscription = null
+    }
+
+    override fun onAbandoned() = Unit
 }
