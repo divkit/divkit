ffa0195e013693fea5d56fad5e4130898f943dc6
pkurchatov <pkurchatov@yandex-team.com>
2026-08-11 11:47:52 +0300

    Removed @Deprecated annotation from ParsingEnvironment commit_hash:75223cad371ffe5ad43341640617ed80463e2958

--- client/android/div-data/src/main/java/com/yandex/div/json/ParsingEnvironment.kt
+++ client/android/div-data/src/main/java/com/yandex/div/json/ParsingEnvironment.kt
@@ -2,8 +2,4 @@ package com.yandex.div.json
 
 import com.yandex.div.serialization.ParsingContext
 
-@Deprecated(
-    message = "Use ParsingContext instead.",
-    replaceWith = ReplaceWith("ParsingContext", "com.yandex.div.serialization.ParsingContext")
-)
 interface ParsingEnvironment : ParsingContext
