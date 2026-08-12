70785ccecd171d30c0d4196858ff95d3c7f6eb14
pkurchatov <pkurchatov@yandex-team.com>
2026-08-12 14:45:29 +0300

    Updated AppMetrica commit_hash:99dde9f294b247e11c7db98c36201cdb278f2373

--- client/android/divkit-demo-app/src/main/java/com/yandex/divkit/demo/utils/MetricaUtils.kt
+++ client/android/divkit-demo-app/src/main/java/com/yandex/divkit/demo/utils/MetricaUtils.kt
@@ -6,6 +6,7 @@ import io.appmetrica.analytics.AdRevenue
 import io.appmetrica.analytics.AppMetrica
 import io.appmetrica.analytics.IReporter
 import io.appmetrica.analytics.Revenue
+import io.appmetrica.analytics.coreapi.event.AppMetricaEvent
 import io.appmetrica.analytics.ecommerce.ECommerceEvent
 import io.appmetrica.analytics.plugins.IPluginReporter
 import io.appmetrica.analytics.plugins.PluginErrorDetails
@@ -13,10 +14,6 @@ import io.appmetrica.analytics.profile.UserProfile
 
 object MetricaUtils {
 
-    /**
-     * Gets AppMetrica reporter for AliceKit components.
-     */
-    @JvmStatic
     fun getReporter(context: Context): IReporter {
         if (BuildConfig.METRICA_API_KEY.isEmpty()) {
             return NoOpReporter
@@ -27,42 +24,38 @@ object MetricaUtils {
     private object NoOpReporter : IReporter {
 
         override fun pauseSession() = Unit
-
         override fun resumeSession() = Unit
 
         override fun reportError(identifier: String, message: String?) = Unit
-
         override fun reportError(identifier: String, message: String?, error: Throwable?) = Unit
-
         override fun reportError(message: String, error: Throwable?) = Unit
 
         override fun reportUnhandledException(exception: Throwable) = Unit
 
+        override fun reportAnr(p0: Map<Thread, Array<StackTraceElement>>) = Unit
+
         override fun reportEvent(eventName: String) = Unit
-
         override fun reportEvent(eventName: String, attributes: Map<String, Any>?) = Unit
-
         override fun reportEvent(eventName: String, jsonValue: String?) = Unit
+        override fun reportEvent(event: AppMetricaEvent) = Unit
 
         override fun reportRevenue(revenue: Revenue) = Unit
-
+        override fun reportAdRevenue(revenue: AdRevenue) = Unit
         override fun reportECommerce(event: ECommerceEvent) = Unit
-
-        override fun setDataSendingEnabled(p0: Boolean) = Unit
-
         override fun reportUserProfile(profile: UserProfile) = Unit
+
+        override fun setDataSendingEnabled(isEnabled: Boolean) = Unit
+        override fun setUserProfileID(profileID: String?) = Unit
 
         override fun sendEventsBuffer() = Unit
 
         override fun getPluginExtension(): IPluginReporter = NoOpPluginReporter
 
-        override fun reportAdRevenue(p0: AdRevenue) = Unit
-
         override fun putAppEnvironmentValue(p0: String, p1: String?) = Unit
+        override fun putErrorEnvironmentValue(p0: String, p1: String?) = Unit
 
         override fun clearAppEnvironment() = Unit
-
-        override fun setUserProfileID(profileID: String?) = Unit
+        override fun clearErrorEnvironment() = Unit
     }
 
     private object NoOpPluginReporter : IPluginReporter {
