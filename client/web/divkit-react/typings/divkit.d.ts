9a964962b9ab2649fbc7756dd4ff707cc6d022ad
4eb0da <4eb0da@yandex-team.com>
2026-08-27 16:47:09 +0300

    Fix divkit-react types commit_hash:7e418d33acaeaa56b0338ff7d2283586866244ee

--- client/web/divkit-react/typings/divkit.d.ts
+++ client/web/divkit-react/typings/divkit.d.ts
@@ -1,8 +1,9 @@
 import type React from 'react';
 import type { Patch } from '@divkitframework/divkit/typings/common.d.ts';
 import { render as clientRender } from '@divkitframework/divkit/client-hydratable';
+import { render as serverRender } from '@divkitframework/divkit/server';
 
-export type DivKitProps = Omit<Parameters<typeof clientRender>[0], 'hydrate' | 'target'>;
+export type DivKitProps = Omit<Parameters<typeof clientRender>[0] & Parameters<typeof serverRender>[0], 'hydrate' | 'target'>;
 
 export interface DivKitHandle {
     applyPatch: (patch: Patch) => boolean;
