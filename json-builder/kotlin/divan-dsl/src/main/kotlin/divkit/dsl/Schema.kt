663b55b5d990b2ad88b7a9f64496b1efe7fcfc74
gulevsky <gulevsky@yandex-team.com>
2026-08-31 13:43:43 +0300

    Remove DivKit DSL deprecated API from portal mapi V2 commit_hash:1ce4c82cabec1aac54b6f5ffb4a5944c9e0ea624

--- json-builder/kotlin/divan-dsl/src/main/kotlin/divkit/dsl/Schema.kt
+++
@@ -1,9 +0,0 @@
-package divkit.dsl
-
-import divkit.dsl.scope.DivScope
-
-@Deprecated(message = "Use method `color(argb: String)` without DivScope receiver", replaceWith = ReplaceWith("color"))
-fun DivScope.color(argb: String): Color = divkit.dsl.color(argb)
-
-@Deprecated(message = "Use method `url(url: String)` without DivScope receiver", replaceWith = ReplaceWith("url"))
-fun DivScope.url(url: String): Url = divkit.dsl.url(url)
