65f05531479faf8e43e60f669f6842cbfba97ba3
i-ts <i-ts@yandex-team.com>
2026-09-21 20:24:05 +0300

    stabilize tap-related UI tests commit_hash:bb652e76c1d2c0048005561fd8f10d4022748f95

--- client/android/divkit-demo-app/src/androidTest/java/com/yandex/div/steps/ClickHandlingTestSteps.kt
+++ client/android/divkit-demo-app/src/androidTest/java/com/yandex/div/steps/ClickHandlingTestSteps.kt
@@ -1,17 +1,25 @@
 package com.yandex.div.steps
 
+import android.os.SystemClock
+import android.view.MotionEvent
+import android.view.View
+import android.view.ViewConfiguration
 import android.view.ViewGroup
 import androidx.test.espresso.Espresso.onView
-import androidx.test.espresso.action.ViewActions.click
-import androidx.test.espresso.action.ViewActions.doubleClick
+import androidx.test.espresso.UiController
+import androidx.test.espresso.ViewAction
 import androidx.test.espresso.action.ViewActions.longClick
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayingAtLeast
 import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.rule.ActivityTestRule
 import com.yandex.div.view.ViewActions
 import com.yandex.test.util.Report.step
 import com.yandex.test.util.StepsDsl
+import org.junit.Assert
+
+private const val MAX_GESTURE_HANDLING_TIME_NS = 48_000_000L
 
 internal fun testClicks(f: ClickHandlingTestSteps.() -> Unit) = f(ClickHandlingTestSteps())
 
@@ -30,12 +38,12 @@ internal open class ClickHandlingTestSteps : DivTestAssetSteps() {
 
     fun click(text: String): Unit =
         step("Click on view with text='$text'") {
-            findView(text).perform(click())
+            findView(text).perform(controlledSingleTap())
         }
 
     fun doubleClick(text: String): Unit =
         step("Double click on view with text='$text'") {
-            findView(text).perform(doubleClick())
+            findView(text).perform(controlledDoubleTap())
         }
 
     fun longClick(text: String): Unit =
@@ -58,5 +66,78 @@ internal class ClickHandlingAssertions {
             } else {
                 ViewActions.waitForView(withText(text), timeout)
             }.check(matches(isDisplayed()))
+    }
+}
+
+private fun controlledDoubleTap() = object : ViewAction {
+
+    override fun getConstraints() = isDisplayingAtLeast(90)
+
+    override fun getDescription() = "double tap with controlled event timing"
+
+    override fun perform(uiController: UiController, view: View) {
+        val firstDownTime = SystemClock.uptimeMillis()
+        val tapDuration = ViewConfiguration.getTapTimeout().toLong() / 2
+        val intervalBetweenTaps = ViewConfiguration.getDoubleTapTimeout().toLong() / 2
+        val firstUpTime = firstDownTime + tapDuration
+        val secondDownTime = firstUpTime + intervalBetweenTaps
+        val secondUpTime = secondDownTime + tapDuration
+        val centerX = view.width / 2f
+        val centerY = view.height / 2f
+
+        // Espresso's Tap.DOUBLE waits on the main looper and can oversleep the double-tap window.
+        val events = listOf(
+            MotionEvent.obtain(firstDownTime, firstDownTime, MotionEvent.ACTION_DOWN, centerX, centerY, 0),
+            MotionEvent.obtain(firstDownTime, firstUpTime, MotionEvent.ACTION_UP, centerX, centerY, 0),
+            MotionEvent.obtain(secondDownTime, secondDownTime, MotionEvent.ACTION_DOWN, centerX, centerY, 0),
+            MotionEvent.obtain(secondDownTime, secondUpTime, MotionEvent.ACTION_UP, centerX, centerY, 0),
+        )
+
+        dispatchWithinFrameBudget(view, "double tap", events)
+        uiController.loopMainThreadUntilIdle()
+    }
+}
+
+private fun controlledSingleTap() = object : ViewAction {
+
+    override fun getConstraints() = isDisplayingAtLeast(90)
+
+    override fun getDescription() = "single tap with controlled event timing"
+
+    override fun perform(uiController: UiController, view: View) {
+        val downTime = SystemClock.uptimeMillis()
+        val upTime = downTime + ViewConfiguration.getTapTimeout().toLong() / 2
+        val centerX = view.width / 2f
+        val centerY = view.height / 2f
+        val events = listOf(
+            MotionEvent.obtain(downTime, downTime, MotionEvent.ACTION_DOWN, centerX, centerY, 0),
+            MotionEvent.obtain(downTime, upTime, MotionEvent.ACTION_UP, centerX, centerY, 0),
+        )
+
+        dispatchWithinFrameBudget(view, "single tap", events)
+        uiController.loopMainThreadUntilIdle()
+    }
+}
+
+private fun dispatchWithinFrameBudget(
+    view: View,
+    gestureName: String,
+    events: List<MotionEvent>,
+) {
+    val elapsedNs = try {
+        val startedAtNs = SystemClock.elapsedRealtimeNanos()
+        events.forEach { event ->
+            view.dispatchTouchEvent(event)
         }
+        SystemClock.elapsedRealtimeNanos() - startedAtNs
+    } finally {
+        events.forEach { event ->
+            event.recycle()
+        }
+    }
+
+    Assert.assertTrue(
+        "$gestureName took $elapsedNs ns; expected < $MAX_GESTURE_HANDLING_TIME_NS ns",
+        elapsedNs < MAX_GESTURE_HANDLING_TIME_NS,
+    )
 }
